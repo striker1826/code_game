d5323974d6f78c4c3b1e7f0fb610bfa9ed2c2475
striker1826 <alstjq123579@daum.net>
2024-03-11 19:52:30 +0900

    feat:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,12 +7,7 @@ export class AppController {
 
   @Get()
   async getHello() {
-    console.log(process.env.DATABASE_HOST);
-    console.log(process.env.DATABASE_USER);
-    console.log(process.env.DATABASE_PASSWORD);
-    console.log(process.env.DATABASE_NAME);
     // return this.appService.test(`function string(input) {
-
     // return input
     // }`);
   }
