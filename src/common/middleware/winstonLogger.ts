921cf05dd96852b69feeb0c794e73ee9c1c1d8d1
striker1826 <alstjq123579@daum.net>
2024-03-22 16:30:56 +0900

    feat: production 모델에서 info 로그 제거

--- src/common/middleware/winstonLogger.ts
+++ src/common/middleware/winstonLogger.ts
@@ -31,7 +31,7 @@ export const winstonLogger = WinstonModule.createLogger({
               }),
             ),
     }),
-    new winstonDaily(dailyOptions('info')),
+    // new winstonDaily(dailyOptions('info')),
     new winstonDaily(dailyOptions('warn')),
     new winstonDaily(dailyOptions('error')),
   ],
