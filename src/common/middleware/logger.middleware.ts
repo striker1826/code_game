38440f1a88f28fc519f0b28a6addeb6c47cc2c12
striker1826 <alstjq123579@daum.net>
2024-03-22 17:00:30 +0900

    fix: logger에서 userAgent 제거 및 body 값 추가

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -7,13 +7,13 @@ export class LoggerMiddleware implements NestMiddleware {
 
   use(req: Request, res: Response, next: NextFunction) {
     // 요청 객체로부터 ip, http method, url, user agent를 받아온 후
-    const { ip, method, originalUrl } = req;
+    const { ip, method, originalUrl, body } = req;
     const userAgent = req.get('user-agent');
 
     // 응답이 끝나는 이벤트가 발생하면 로그를 찍는다.
     res.on('finish', () => {
       const { statusCode } = res;
-      this.logger.log(`${ip} ${method} ${originalUrl} ${statusCode} ${userAgent}`);
+      this.logger.log(`${ip} ${method} ${originalUrl} ${statusCode} ${JSON.stringify(body)}`);
     });
 
     next();
